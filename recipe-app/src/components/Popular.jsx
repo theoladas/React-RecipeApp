02d3b40dfa3e990afe1e94d245e7fff9504e8513
Theo <theodorosladasmail@gmail.com>
2022-06-13 20:42:05 +0100

    Created - state and loop through Popular API recipes to get each recipe Title

--- recipe-app/src/components/Popular.jsx
+++ recipe-app/src/components/Popular.jsx
@@ -1,23 +1,33 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const Popular = () => {
+  // create a state for the popular recipes
+  const [popular, setPopular] = useState([]);
+
   // call the function when the page loads
   useEffect(() => {
     getPopular();
   }, []);
 
-  // Fetch Popular recipies from API
+  // Fetch Popular recipes from API
   const getPopular = async () => {
     const api = await fetch(
       `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
     );
     // to get the data in json format
     const data = await api.json();
-    console.log(data);
+    // set setPopular value
+    setPopular(data.recipes);
   };
   return (
-    <div>
-      <h1>Popular</h1>
+    <div key={recipe.id}>
+      {popular.map((recipe) => {
+        return (
+          <div>
+            <p>{recipe.title}</p>
+          </div>
+        );
+      })}
     </div>
   );
 };
