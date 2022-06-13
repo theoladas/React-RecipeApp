615146e247d6c715324cd3e1a954034d93658e86
Theo <theodorosladasmail@gmail.com>
2022-06-13 20:26:46 +0100

    Added - API function to grab the Popular recipes

--- recipe-app/src/components/Popular.jsx
+++ recipe-app/src/components/Popular.jsx
@@ -1,4 +1,20 @@
+import { useEffect } from "react";
+
 const Popular = () => {
+  // call the function when the page loads
+  useEffect(() => {
+    getPopular();
+  }, []);
+
+  // Fetch Popular recipies from API
+  const getPopular = async () => {
+    const api = await fetch(
+      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
+    );
+    // to get the data in json format
+    const data = await api.json();
+    console.log(data);
+  };
   return (
     <div>
       <h1>Popular</h1>
