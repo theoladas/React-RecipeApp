26a89f298543df7e1ae36900fc25ff14ff53c0b6
Theo <theodorosladasmail@gmail.com>
2022-06-17 00:16:12 +0100

    Added - styles for NavLinks / Created - active class styles

--- recipe-app/src/components/Category.jsx
+++ recipe-app/src/components/Category.jsx
@@ -7,22 +7,22 @@ import { NavLink } from "react-router-dom";
 const Category = () => {
   return (
     <NavList>
-      <NavLink to={"/Cuisine/Italian"}>
+      <StyleLink to={"/Cuisine/Italian"}>
         <FaPizzaSlice />
         <h4>Italian</h4>
-      </NavLink>
-      <NavLink to={"/Cuisine/American"}>
+      </StyleLink>
+      <StyleLink to={"/Cuisine/American"}>
         <FaHamburger />
         <h4>American</h4>
-      </NavLink>
-      <NavLink to={"/Cuisine/Thai"}>
+      </StyleLink>
+      <StyleLink to={"/Cuisine/Thai"}>
         <GiNoodles />
         <h4>Thai</h4>
-      </NavLink>
-      <NavLink to={"/Cuisine/Chinese"}>
+      </StyleLink>
+      <StyleLink to={"/Cuisine/Chinese"}>
         <GiChopsticks />
         <h4>Chinese</h4>
-      </NavLink>
+      </StyleLink>
     </NavList>
   );
 };
@@ -34,5 +34,39 @@ const NavList = styled.div`
   justify-content: center;
   margin: 2rem 0rem;
 `;
+// NavLinks styles
+const StyleLink = styled(NavLink)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  margin-right: 2rem;
+  text-decoration: none;
+  background: linear-gradient(35deg, #494949, #313131);
+  width: 6rem;
+  height: 6rem;
+  cursor: pointer;
+  tranform: scale(0.8);
 
+  h4 {
+    color: white;
+    font-size: 0.8rem;
+    margin-top: 0.2rem;
+  }
+  svg {
+    color: white;
+    font-size: 1.5rem;
+  }
+  // create a class , when we click an icon to stay active
+  &.active {
+    background: linear-gradient(to right, #f27121, #e94057);
+  }
+  svg {
+    color: white;
+  }
+  h4 {
+    color: white;
+  }
+`;
 export default Category;
