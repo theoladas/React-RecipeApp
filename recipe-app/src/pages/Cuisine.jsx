3204c690bb23f1e664458c033a7b6d15e374f194
Theo <theodorosladasmail@gmail.com>
2022-06-16 20:17:27 +0100

    Added - imports for Cuisine.jsx

--- recipe-app/src/pages/Cuisine.jsx
+++ recipe-app/src/pages/Cuisine.jsx
@@ -1,5 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
+
 const Cuisine = () => {
-  return <div>Cuisine</div>;
+  return <div></div>;
 };
 
 export default Cuisine;
