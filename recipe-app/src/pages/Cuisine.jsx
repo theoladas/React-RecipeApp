61145c14f1b937432a950f6a33307de52f6435a9
Theo <theodorosladasmail@gmail.com>
2022-06-16 22:58:32 +0100

    Created - state and fetch Cuisine API

--- recipe-app/src/pages/Cuisine.jsx
+++ recipe-app/src/pages/Cuisine.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 
 const Cuisine = () => {
+  //import state
+  const [cuisine, setCuisine] = useState([]);
+  // get cuisine API function
+  const getCuisine = async (name) => {
+    const data = await fetch(
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&cuisine=${name}`
+    );
+    const recipes = await data.JSON();
+    setCuisine(recipes.results);
+  };
   return <div></div>;
 };
 
