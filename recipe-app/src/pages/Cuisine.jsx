c6ce539d80062ef40cf6884e4b6fc77c8334d9d5
Theo <theodorosladasmail@gmail.com>
2022-06-16 23:14:27 +0100

    Added - useEffect and invoke the function using params

--- recipe-app/src/pages/Cuisine.jsx
+++ recipe-app/src/pages/Cuisine.jsx
@@ -1,19 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { motion } from "framer-motion";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 const Cuisine = () => {
   //import state
   const [cuisine, setCuisine] = useState([]);
+  // useParams
+  let params = useParams();
   // get cuisine API function
   const getCuisine = async (name) => {
     const data = await fetch(
       `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&cuisine=${name}`
     );
-    const recipes = await data.JSON();
+    const recipes = await data.json();
     setCuisine(recipes.results);
   };
+
+  // invoke the function
+  useEffect(() => {
+    // we pass the function and we add the (cuisine) based in the path's url and whatever add as parameter it will go to the API's {name} and it will updte the recipes
+    getCuisine(params.type);
+  }, [params.type]);
   return <div></div>;
 };
 
