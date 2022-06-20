228acc710a69cceb99579081c9b2cfb183f2ac98
Theo <theodorosladasmail@gmail.com>
2022-06-20 17:04:43 +0100

    Added - code to get recipe id details from API

--- recipe-app/src/pages/Recipe.jsx
+++ recipe-app/src/pages/Recipe.jsx
@@ -4,7 +4,26 @@ import styled from "styled-components";
 import { useParams } from "react-router-dom";
 
 const Recipe = () => {
-  return <div>Recipe</div>;
+  // use params
+  let params = useParams();
+  //   create state
+  const [details, setDetails] = useState({});
+  // fetch and get recipe.id from the API
+  const fetchDetails = async () => {
+    // GET the data
+    const data = await fetch(
+      `https://api.spoonacular.com/recipes/${params.name}/information?apiKey=${process.env.REACT_APP_API_KEY}`
+    );
+    // SAVE the data
+    const detailData = await data.json();
+    setDetails(detailData);
+  };
+  //   Write useEffect function
+  useEffect(() => {
+    fetchDetails();
+  }, [params.name]);
+  // Render everything
+  return <div>{details.title}</div>;
 };
 
 export default Recipe;
