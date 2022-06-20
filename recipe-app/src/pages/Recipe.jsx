dbcd26d313566c58411ca7a6301771e92a90cb4a
Theo <theodorosladasmail@gmail.com>
2022-06-20 20:53:06 +0100

    Render - recipe's instructions and summary text

--- recipe-app/src/pages/Recipe.jsx
+++ recipe-app/src/pages/Recipe.jsx
@@ -45,6 +45,10 @@ const Recipe = () => {
         >
           Ingredients
         </Button>
+        <div>
+          <h3 dangerouslySetInnerHTML={{ __html: details.summary }}></h3>
+          <h3 dangerouslySetInnerHTML={{ __html: details.instructions }}></h3>
+        </div>
       </Info>
     </DetailWrapper>
   );
