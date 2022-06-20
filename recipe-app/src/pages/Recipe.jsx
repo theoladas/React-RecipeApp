676a819a4685cf80c573f7c20be7528f7d53886a
Theo <theodorosladasmail@gmail.com>
2022-06-20 19:17:52 +0100

    Added - styles on Recipe.jsx and render code

--- recipe-app/src/pages/Recipe.jsx
+++ recipe-app/src/pages/Recipe.jsx
@@ -23,7 +23,49 @@ const Recipe = () => {
     fetchDetails();
   }, [params.name]);
   // Render everything
-  return <div>{details.title}</div>;
+  return (
+    <DetailWrapper>
+      <div>
+        <h2>{details.title}</h2>
+        <img src={details.image} alt={details.title} />
+      </div>
+      <Info>
+        <Button>Instructions</Button>
+        <Button>Ingredients</Button>
+      </Info>
+    </DetailWrapper>
+  );
 };
 
+// Styles
+const DetailWrapper = styled.div`
+  margin-top: 10rem;
+  margin-bottom: 5rem;
+  display: flex;
+  &.active {
+    background: linear-gradient(35deg, #494949, #313131);
+    color: white;
+  }
+  h2 {
+    margin-bottom: 2rem;
+  }
+  ul {
+    margin-top: 2rem;
+  }
+  li {
+    font-size: 1.2rem;
+    line-height: 2.5rem;
+  }
+`;
+const Button = styled.button`
+  padding: 1rem 2rem;
+  color: #313131;
+  background: white;
+  border: 2px solid black;
+  margin-right: 2rem;
+  font-weight: 600;
+`;
+const Info = styled.div`
+  margin-left: 10rem;
+`;
 export default Recipe;
