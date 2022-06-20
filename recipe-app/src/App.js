43af9251e3335ea12c73f68ef2eb3ec5f67fffa2
Theo <theodorosladasmail@gmail.com>
2022-06-20 21:50:43 +0100

    Added - Navigation and Icon to the App.js

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -2,15 +2,38 @@ import Category from "./components/Category";
 import Pages from "./pages/Pages";
 import Search from "./components/Search";
 import { BrowserRouter } from "react-router-dom";
-
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { GiKnifeFork } from "react-icons/gi";
 const App = () => {
   return (
-    <BrowserRouter>
-      <Search />
-      <Category />
-      <Pages />
-    </BrowserRouter>
+    <div className="App">
+      <BrowserRouter>
+        <Nav>
+          <GiKnifeFork />
+          <Logo to={"/"}>delicious</Logo>
+        </Nav>
+        <Search />
+        <Category />
+        <Pages />
+      </BrowserRouter>
+    </div>
   );
 };
 
+const Logo = styled(Link)`
+  text-decoration: none;
+  font-size: 1.5rem;
+  font-weight: 400;
+  font-family: "Lobster", cursive;
+`;
+const Nav = styled.div`
+  padding: 4rem 0rem;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  svg {
+    font-size: 2rem;
+  }
+`;
 export default App;
