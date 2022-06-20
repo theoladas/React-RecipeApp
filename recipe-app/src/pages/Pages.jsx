09c8009149582712a112f8683f9f6af612947d28
Theo <theodorosladasmail@gmail.com>
2022-06-20 22:25:57 +0100

    Added - frame motion animate presence

--- recipe-app/src/pages/Pages.jsx
+++ recipe-app/src/pages/Pages.jsx
@@ -3,16 +3,21 @@ import Home from "./Home";
 import Cuisine from "./Cuisine";
 import Searched from "./Searched";
 import Recipe from "./Recipe";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
 
 const Pages = () => {
+  // Add location for Routes
+  const location = useLocation();
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/Cuisine/:type" element={<Cuisine />} />
-      <Route path="/Searched/:search" element={<Searched />} />
-      <Route path="/Recipe/:name" element={<Recipe />}></Route>
-    </Routes>
+    <AnimatePresence exitBeforeEnter>
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<Home />} />
+        <Route path="/Cuisine/:type" element={<Cuisine />} />
+        <Route path="/Searched/:search" element={<Searched />} />
+        <Route path="/Recipe/:name" element={<Recipe />}></Route>
+      </Routes>
+    </AnimatePresence>
   );
 };
 
