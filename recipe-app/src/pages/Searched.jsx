01eb14b3fe77de357e0f281c436db14084b73156
Theo <theodorosladasmail@gmail.com>
2022-06-20 16:13:40 +0100

    Added - Styles for Searched.jsx and Render everything out

--- recipe-app/src/pages/Searched.jsx
+++ recipe-app/src/pages/Searched.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import styled from "styled-components";
+
 const Searched = () => {
   // create searched state
   const [searchedRecipes, setSearchedRecipes] = useState([]);
@@ -22,7 +24,37 @@ const Searched = () => {
     getSearched(params.search);
     // in order to updates everytime we searched we add params.search to the array
   }, [params.search]);
-  return <div>Searched</div>;
+
+  return (
+    <Grid>
+      {searchedRecipes.map((item) => {
+        return (
+          <Card key={item.id}>
+            <img src={item.image} alt={item.title}></img>
+            <h4>{item.title}</h4>
+          </Card>
+        );
+      })}
+    </Grid>
+  );
 };
 
+// create a responsive Grid
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+  grid-gap: 3rem;
+`;
+const Card = styled.div`
+  img {
+    width: 100%;
+    border-radius: 2rem;
+    a {
+      text-decoration: none;
+    }
+  }
+  h4 {
+    text-align: center;
+  }
+`;
 export default Searched;
